9cb7f404413251b6535eecc87c9004e1f973ff42
kapil524 <kapil.hcst1@gmail.com>
2024-01-16 09:50:15 +0530

    new-data

---
+++ resource.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+
+  dynamic ingress {
+    for_each = var.port_numbers
+    content {
+    from_port   = ingress.value
+    to_port     = ingress.value
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ var.tf
@@ -0,0 +1,3 @@
+variable "port_numbers" {
+  type = list
+}
